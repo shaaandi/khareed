6f58aca766e8f681e7f751930de36054618eec86
Shaaandi <deviloversaad@gmail.com>
2019-08-30 15:06:22 +0500

    Debugging Deployment Issue

--- index.js
+++ index.js
@@ -60,8 +60,10 @@ require('./routes/seedDataRoutes')(app);
 require('./routes/serviceRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-
+if(process.env.NODE_ENV) console.log(process.env.NODE_ENV);
+else console.log('Not in production');
 if (process.env.NODE_ENV === 'production') {
+    console.log('I am called')
     app.use(express.static('client/build'))
 
     const path = require('path');
